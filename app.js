2aca4402c5a9eb57bd3db139f6352ef683faa690
Paul <wesman687@gmail.com>
2024-07-19 02:55:16 -0400

    twoSum

---
+++ app.js
@@ -0,0 +1,33 @@
+/** Two Sum **
+ * 
+ * Find two numbers in the given array that add up to `target`
+ * and return the indices of those two numbers in the array
+ * 
+ * @example
+ * twoSum([1, 2, 4, 9], 13) -> [2, 3]
+ * twoSum([10, 20], 30) -> [0, 1]
+ * twoSum([3, 2, 4], 6) -> [1, 2]
+ * 
+ */
+const mapDifferenceToIndex = new Map()
+
+function twoSum(nums, target){
+    
+    for (let i = 0; i < nums.length - 1; i++)
+        if (mapDifferenceToIndex.has(nums[i])){
+            return [mapDifferenceToIndex.get(nums[i], i)]
+        }
+        const difference = target - nums[i]
+        mapDifferenceToIndex.set(difference, i)
+
+
+    /*
+    for (let i = 0; i < nums.length - 1; ++i){
+        for(let j = i + 1; j < nums.length; ++j){
+            if (nums[i] + nums[j] == target)
+                console.log(i,j, target)
+        }
+    }*/
+}
+
+console.log(twoSum([1, 2, 4, 9], 13))
