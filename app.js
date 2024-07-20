34bb69ef8cde8d3ff3afb8d1cc62b589fd2d8d05
Paul <wesman687@gmail.com>
2024-07-20 00:38:08 -0400

    flatten arrays

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
-/** Container with Most Water **
+/** Flatten Array **
  * 
- * Given an array 'heights' which represents the heights of blocks
- * on a container, find the maximum area of water that can be stored
- * within these blocks. 
+ * Given an array 'nums' that can have nested arrays inside it,
+ * flatten the array. 
+ *  
  * 
  * @example
- * maxWater([1, 5, 4, 3]) -> 6
- * maxWater([1, 5, 6, 3, 4, 2]) -> 12
- * maxWater([4, 3, 2, 1, 4]) -> 16
+ * flatten([1, 2, 3, [4, 5]]) -> [1, 2, 3, 4, 5]
+ * flatten([1, 2, 3, [4, [5]]]) -> [1, 2, 3, 4, 5]
+ * flatten([1, [2, 3, [4, [5]]]]) -> [1, 2, 3, 4, 5]
  * 
  */
-function maxWater(heights) {
-    let l = 0
-    let r = heights.length - 1
-    let maxArea = 0
-
-    while (l<r) {
-        let width = r - l 
-        let height = Math.min(heights[l], heights[r])
-        let area = width * height
-        console.log(area, height, width, l, r)
-        if (heights[l] < heights[r]){
-            l += 1
-        }
-        else {
-            r -= 1
-        }
-
-        if (area > maxArea) {
-            maxArea = area
-        }
+const results = []
+function flatten(nums) {
+    for (elem of nums) {
+        if(Array.isArray(elem)){
+            flatten(elem)
+        }else{
+            results.push(elem)
+        } 
+        
     }
-    return maxArea
-    
-  
+  return results
 }
 
-console.log(maxWater([1, 5, 6, 3, 4, 2]));
+
+console.log(flatten([1, [2, 3, [4, [5]]]]));
