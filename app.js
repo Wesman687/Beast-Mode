230192f46f6d65caf652c1822a7f05f6474bc474
Paul <wesman687@gmail.com>
2024-07-19 23:56:48 -0400

    more binary search

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
-/** Minimum Number in Rotated Sorted Array **
+/** Search in Rotated Sorted Array **
  * 
- * Given a rotated sorted array, return the smallest 
- * number in the array.
+ * Given a rotated sorted array and a target, 
+ * return the index of the number in the array 
+ * which equals to the target.
  * 
  * Note: Your solutions' time complexity must be faster than O(n). 
  * 
  * @example
- * findMinimum([5, 4, 1, 2]) -> 1
- * findMinimum([7, 9, 12, 3, 4]) -> 3
- * findMinimum([3, 4, 2, 0]) -> 0
+ * search([5, 4, 1, 2, 3], 2) -> 3
+ * search([7, 9, 12, 3, 4], 7) -> 0
+ * search([3, 4, 2, 0], 4) -> 1
  * 
  */
-function findMinimum(nums) {
-  let l = 0
-  let r = nums.length - 1
-  while (l<r){
-    let mid = Math.floor((l + r)/ 2)
-    if (nums[mid] > nums[l]){
-        l = mid + 1
+function search(nums, target) {
+    let l = 0
+    let r = nums.length - 1
+    while (l<r){
+        let mid = Math.floor((l + r)/ 2)
+        if (nums[mid] === target) {
+            return mid
+        }
+        if (nums[mid]< nums[r]){
+            if (target < nums[mid] || target > nums[r]){
+                r = mid - 1
+            } 
+            else {
+                l = mid + 1
+            }
+        }
+        else  {
+            if (target > nums[mid] || target < nums[l]){
+                l = mid + 1
+            }
+            else {
+                r = mid - 1
+            }
+        }
     }
-    else {
-        r = mid
-    }
-  }
-  return l
+  return "not found"
   
 }
 
-console.log(findMinimum([7, 9, 12, 3, 4]));
+console.log(search([3, 4, 2, 0], 4));
--- questions/binarySearch/index.js
+++ questions/binarySearch/index.js
@@ -13,7 +13,21 @@
  */
 
 const binarySearch = (nums, target) => {
-
+    let l = 0;
+    let r = nums.length - 1;
+    let i = 0;
+    while (l<=r) {     
+      mid = Math.floor((l + r) / 2); 
+      if (nums[mid] === target){
+          return mid
+      }
+      else if (nums[mid] > target) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return"No matches";
 
 }
 
